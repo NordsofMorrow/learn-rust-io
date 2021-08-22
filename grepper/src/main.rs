a5fa868b599f60bd77f291214e67f659e483e63b
NordsofMorrow <nordsofmorrow@protonmail.com>
2021-08-22 00:38:47 -0400

    continuing through docs

--- grepper/src/main.rs
+++ grepper/src/main.rs
@@ -1,13 +1,29 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    let config = parse_configs(&args);
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    let contents: String = fs::read_to_string(config.filename).expect("Unable to read from file");
+
+    println!("Contents are as follows:\n\n{}", contents);
+}
+
+struct Config<'a> {
+    query: &'a String,
+    filename: &'a String,
+}
+
+fn parse_configs(args: &[String]) -> Config {
     let query = &args[1];
     let filename = &args[2];
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    Config { query, filename }
 }
 
 #[cfg(test)]
