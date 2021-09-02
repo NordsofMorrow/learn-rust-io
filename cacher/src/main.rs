cfb97c39527a5deda453195832417be4cc70de89
NordsofMorrow <nordsofmorrow@protonmail.com>
2021-09-01 23:27:02 -0400

    add cacher example

---
+++ cacher/src/main.rs
@@ -0,0 +1,53 @@
+use std::thread;
+use std::time::Duration;
+
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+
+    let mut expensive_result = Cacher::new(|num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
+
+    let values = 21;
+
+    {
+        let values = 32;
+        println!("{}", expensive_result.value(values));
+        println!("{}", expensive_result.value(values));
+    }
+
+    println!("{}", expensive_result.value(values));
+}
